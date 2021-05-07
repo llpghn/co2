01ec18ebc44f21087917094a0cdd399376f54a94
llpghn <patrickberendes@googlemail.com>
2021-05-08 00:15:49 +0200

    cleanup

--- scripts/copy_scripts.sh
+++ scripts/copy_scripts.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+# Autor: Patrick, Berendes
+# Created on: 2021-05-08
+
 # Copies all necessary scripts on the target system using scp. 
+# All handler that are needed on the target system have to be placed in the 
+# folder /handler.  
+# Danach sind die Daten dann auch dem System unter /usr/handler
 
 echo "Skript-copy-tool"
 echo "Current working directory on local machine $(PWD)"
 
-
-ssh pi@raspberrypi -T << 'EOF'
-  mkdir -p /usr/handler
-  cd /usr/handler
-  git clone https://github.com/llpghn/co2
+ssh -T pi@raspberrypi <<'EOF'
+  sudo mkdir -p /usr/temp
+  sudo mkdir -p /usr/handler
+  cd /usr/temp
+  sudo git clone https://github.com/llpghn/co2
+  sudo cp -r /usr/temp/co2/handler/ /usr/
+  sudo rm -r /usr/temp
 EOF
