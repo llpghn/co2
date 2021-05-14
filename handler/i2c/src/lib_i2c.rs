b37e78d35dddb44c629e3cc6cba5d0fb3c2bc425
llpghn <patrickberendes@googlemail.com>
2021-05-14 23:29:41 +0200

    1

--- handler/i2c/src/lib_i2c.rs
+++ handler/i2c/src/lib_i2c.rs
@@ -11,7 +11,7 @@ pub mod lib_i2c{
   use rppal::i2c::I2c;
   use std::error::Error;
 
-  const ADDR_BME280: u16 = 0x77;
+  const ADDR_BME280: u16 = 0x76;
 
   // Helper functions
   fn bcd2dec(bcd: u8) -> u8 {
