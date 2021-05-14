1d117559c40d1ca1ff211fa5d9dd2a38c9a740cc
llpghn <patrickberendes@googlemail.com>
2021-05-15 00:16:05 +0200

    1

--- handler/i2c/src/lib_i2c.rs
+++ handler/i2c/src/lib_i2c.rs
@@ -11,6 +11,22 @@ pub mod lib_i2c{
   use rppal::i2c::I2c;
   use std::error::Error;
 
+/*
+bcd2dec
+*/
+
+  struct bme260{
+    addr: u16 = 0x76;
+    hum_lsb : u8 = 0xFE;
+    hum_msb : u8 = 0xFD;
+    temp_xlsb : u8 = 0xFC;
+    temp_lsb: u8 = 0xFB;
+    temp_msb : u8 = 0xFA;
+    press_xlsb : u8 = 0xF9;
+    press_lsb : u8 = 0xF8;
+    press_msb : u8 = 0xF7;
+  }
+
   const ADDR_BME280: u16 = 0x76;
 
   // Helper functions
@@ -26,9 +42,10 @@ pub mod lib_i2c{
   pub fn scan() -> Result<(), Box<dyn Error>> {
     println!("Start scanning I2C-Bus - 101");
     let mut i2c = I2c::new()?;
-    i2c.set_slave_address(ADDR_BME280)?;
-    let command: [u8; 1] = [0xF7];
-
+    i2c.set_slave_address(bme260.addr)?;
+    
+    //let command: [u8; 1] = [0xF7];
+    let command: [u8; 1] = [bme260.hum_msb];
     let written_bytes: usize = i2c.write(&command)?;
     println!("Total Bytes send: {}", written_bytes);
 
