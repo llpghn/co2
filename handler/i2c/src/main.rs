8834baa6d4b757d8966b1fe0ed3e9bb3f0671327
llpghn <patrickberendes@googlemail.com>
2021-05-16 00:09:54 +0200

    a

--- handler/i2c/src/main.rs
+++ handler/i2c/src/main.rs
@@ -21,9 +21,9 @@ struct CollectedData {
 
 impl CollectedData {
     fn getTemperatureMessage(&self, topic: &mut String, message: &mut String) {
-        let mut my_topic: String::from("/sensor/value/temperature");
+        let mut my_topic = String::from("/sensor/value/temperature");
         *topic = my_topic;
-        let mut my_message: self.temperature.ToString();
+        let mut my_message = self.temperature.ToString();
         *message = my_message;
     }
 }
