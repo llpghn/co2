a5056ee6b8a9abdc024c32a45dbee26b667d9404
llpghn <patrickberendes@googlemail.com>
2021-05-15 23:16:52 +0200

    .

--- handler/i2c/src/bme280.rs
+++ handler/i2c/src/bme280.rs
@@ -3,8 +3,13 @@ pub mod bme280{
   use linux_embedded_hal::{Delay, I2cdev};
   use bme280::BME280;
 
+  pub struct Data{
+    let temperature: i32,
+    let humidity: i32,
+    let pressure: i32,
+  }
 
-  fn init() -> BME280<I2C, D>::Measure{
+  fn get() -> Data{
     let i2c_bus = I2cdev::new("/dev/i2c-1").unwrap();
     // initialize the BME280 using the primary I2C address 0x76
     let mut bme280 = BME280::new_primary(i2c_bus, Delay);
@@ -22,8 +27,12 @@ pub mod bme280{
     // measure temperature, pressure, and humidity
     let measurements = bme280.measure().unwrap();
     
-    measurements
-
+    let d = Data{
+      temperature : measurements.temperature,
+      humidity : measurements.humidity,
+      pressure : measurements.pressure,
+    }
+    d
     //println!("Relative Humidity = {}%", measurements.humidity);
     //println!("Temperature = {} deg C", measurements.temperature);
     //println!("Pressure = {} pascals", measurements.pressure);
