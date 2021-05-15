9e99db0c9b4ed05ca8394a7a8f7629a674e1f50f
llpghn <patrickberendes@googlemail.com>
2021-05-15 22:24:15 +0200

    Switching to BME280 Crate

--- handler/i2c/src/main.rs
+++ handler/i2c/src/main.rs
@@ -32,7 +32,8 @@
 //mod lib_mqtt;
 //mod lib_i2c;
 
-mod lib_i2c;
+use linux_embedded_hal::{Delay, I2cdev};
+use bme280::BME280;
 /////////////////////////////////////////////////////////////////////////////
 
 
@@ -46,7 +47,30 @@ fn main() {
     //lib_mqtt::lib_mqtt::send_msg_temp(&cli);
     // Disconnect from the broker
     //cli.disconnect(None).unwrap();
-    lib_i2c::lib_i2c::scan();
-    lib_i2c::lib_i2c::read_temp_register();
+
+    
+    // using Linux I2C Bus #1 in this example
+    let i2c_bus = I2cdev::new("/dev/i2c-1").unwrap();
+    
+    // initialize the BME280 using the primary I2C address 0x76
+    let mut bme280 = BME280::new_primary(i2c_bus, Delay);
+    
+    // or, initialize the BME280 using the secondary I2C address 0x77
+    // let mut bme280 = BME280::new_secondary(i2c_bus, Delay);
+    
+    // or, initialize the BME280 using a custom I2C address
+    // let bme280_i2c_addr = 0x88;
+    // let mut bme280 = BME280::new(i2c_bus, bme280_i2c_addr, Delay);
+    
+    // initialize the sensor
+    bme280.init().unwrap();
+    
+    // measure temperature, pressure, and humidity
+    let measurements = bme280.measure().unwrap();
+    
+    println!("Relative Humidity = {}%", measurements.humidity);
+    println!("Temperature = {} deg C", measurements.temperature);
+    println!("Pressure = {} pascals", measurements.pressure);
+
 
 }
